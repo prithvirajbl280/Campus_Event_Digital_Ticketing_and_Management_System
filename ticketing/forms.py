42817553f1e73a65fa5243b5bf7d79bdc9977c92
prithvirajbl280 <prithvirajbl14@gmail.com>
2025-08-28 19:32:23 +0530

    Update forms.py

--- ticketing/forms.py
+++ ticketing/forms.py
@@ -4,10 +4,24 @@ from .models import Registration, TicketConfirmation
 class RegistrationForm(forms.ModelForm):
     class Meta:
         model = Registration
-        fields = ['name', 'srn', 'prn', 'year', 'email', 'phone']
+        fields = ['name', 'srn', 'prn', 'year', 'email', 'phone', 'hostelite', 'hostel_block', 'room_number']
         widgets = {
-            'year': forms.Select()
+            'year': forms.Select(),
+            'hostelite': forms.Select(),  # ChoiceField widget for Yes/No
         }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        hostelite = cleaned_data.get('hostelite')
+
+        if hostelite == 'Yes':
+            if not cleaned_data.get('hostel_block'):
+                self.add_error('hostel_block', "Hostel block/unit is required if hostelite is Yes.")
+            if not cleaned_data.get('room_number'):
+                self.add_error('room_number', "Room number is required if hostelite is Yes.")
+
+        return cleaned_data
+
 
 class TicketConfirmationForm(forms.ModelForm):
     class Meta:
