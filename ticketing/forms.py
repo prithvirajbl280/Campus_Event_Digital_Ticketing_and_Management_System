1746e47f1566863c56e1204ffd164b2600effe92
prithvirajbl280 <prithvirajbl14@gmail.com>
2025-09-04 15:14:53 +0530

    Update forms.py

--- ticketing/forms.py
+++ ticketing/forms.py
@@ -1,26 +1,33 @@
 from django import forms
 from .models import Registration, TicketConfirmation
 
+
 class RegistrationForm(forms.ModelForm):
     class Meta:
         model = Registration
         fields = ['name', 'srn', 'prn', 'year', 'email', 'phone', 'hostelite', 'hostel_block', 'room_number']
         widgets = {
-            'year': forms.Select(),
-            'hostelite': forms.Select(),  # ChoiceField widget for Yes/No
+            'year': forms.Select(attrs={'class': 'form-select'}),
+            'hostelite': forms.Select(attrs={'class': 'form-select'}),
+            'name': forms.TextInput(attrs={'class': 'form-input'}),
+            'srn': forms.TextInput(attrs={'class': 'form-input'}),
+            'prn': forms.TextInput(attrs={'class': 'form-input'}),
+            'email': forms.EmailInput(attrs={'class': 'form-input'}),
+            'phone': forms.TextInput(attrs={'class': 'form-input'}),
+            'hostel_block': forms.TextInput(attrs={'class': 'form-input'}),
+            'room_number': forms.TextInput(attrs={'class': 'form-input'}),
         }
 
     def clean(self):
         cleaned_data = super().clean()
         hostelite = cleaned_data.get('hostelite')
-
         if hostelite == 'Yes':
             if not cleaned_data.get('hostel_block'):
                 self.add_error('hostel_block', "Hostel block/unit is required if hostelite is Yes.")
             if not cleaned_data.get('room_number'):
                 self.add_error('room_number', "Room number is required if hostelite is Yes.")
-
         return cleaned_data
+
 
 
 class TicketConfirmationForm(forms.ModelForm):
